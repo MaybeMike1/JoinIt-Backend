bc54c26ff256f826dc5d156fea5e7491be0207de
MaybeMike1 <57484151+MaybeMike1@users.noreply.github.com>
2024-01-16 08:56:33 +0100

    program

--- JoinIt-Backend/Program.cs
+++ JoinIt-Backend/Program.cs
@@ -1,54 +1,72 @@
+using JoinIt_Backend;
 using JoinIt_Backend.Features.Activity;
 using JoinIt_Backend.Features.Authentication;
-using JoinIt_Backend.Services;
+using JoinIt_Backend.Features.Payment;
 using JoinIt_Backend.Shared;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 
-var builder = WebApplication.CreateBuilder(args);
 
-var config = builder.Configuration;
-// Add services to the container.
-builder.Services.AddShared(config);
-builder.Services.AddAuth();
-builder.Services.AddActivity();
-//builder.Services.AddScoped<IAuthProvider, AuthProvider>();
-//builder.Services.AddScoped<ICryptService, CryptService>();
-//builder.Services.AddScoped<IActivityContextProvider, ActivityContextProvider>();
-//builder.Services.AddScoped<IUserService, UserService>();
+public class Program
+{
+    public static void Main(string[] args)
+        => CreateHostBuilder(args).Build().Run();
+    
+
+    public static IHostBuilder CreateHostBuilder(string[] args)
+        => Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+                
+            });
+}
+
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//var config = builder.Configuration;
+//// Add services to the container.
+//builder.Services.AddShared(config);
+//builder.Services.AddAuth();
+//builder.Services.AddActivity();
+//builder.Services.AddPaymentService();
+
 //builder.Services.AddScoped<DatabaseContext>();
-builder.Services.AddControllers();
-//builder.Services.AddDbContext<DatabaseContext>(opt =>
+//builder.Services.AddDbContext<DatabaseContext>( opt =>
 //{
 //    opt.UseSqlServer(config.GetConnectionString("DefaultConnection"));
 //});
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddAuthentication(x =>
-{
-    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(x =>
-{
-    x.TokenValidationParameters = new TokenValidationParameters();
-});
-var app = builder.Build();
+//builder.Services.AddControllers();
 
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
 
-app.UseHttpsRedirection();
+//builder.Services.AddAuthentication(x =>
+//{
+//    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+//}).AddJwtBearer(x =>
+//{
+//    x.TokenValidationParameters = new TokenValidationParameters();
+//});
+//var app = builder.Build();
 
-app.UseAuthorization();
 
-app.MapControllers();
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
 
-app.Run();
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
